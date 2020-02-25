6ecc1999d3e6b9f5702e69361ca3597af6c90b22
Matthew Man <mman@ucvts.org>
2020-02-25 08:30:55 -0500

    removeword readded

---
+++ src/util/RemoveWord.java
@@ -0,0 +1,16 @@
+package util;
+
+import javax.swing.JPanel;
+import graphical.TxtPnl;
+
+public class RemoveWord {
+	
+	private TxtPnl txtPnl;
+	
+	public void removeWord(TxtPnl txtPn) {
+		System.out.println("Remove word");
+		this.txtPnl.appendText("Remove word");
+	}
+	
+	
+}
